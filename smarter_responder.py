56ab3482ab3e9aae92cef9d883909f4b26d368f0
Chris Raff <crharfifs@gmail.com>
2017-11-05 01:45:31 -0500

    Scoring considers more than most recent sentence :night_with_stars:

--- smarter_responder.py
+++ smarter_responder.py
@@ -39,12 +39,6 @@ else:
 
 stopwords = """a about above after again against all am an and any are aren't as at be because been before being below between both but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours  ourselves out over own same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're you've your yours yourself yourselves""".split()
 
-
-def not_tqdm(x):
-    # return tqdm(x)
-    return x
-
-
 def argmax(scores):
     curr_max = 0
     curr_i = 0
@@ -52,7 +46,7 @@ def argmax(scores):
         if score > curr_max:
             curr_max = score
             curr_i = i
-    return curr_i, score
+    return curr_i
 
 
 # class POSifiedText(markovify.Text):
@@ -109,13 +103,20 @@ else:
 # for i in range(3):
 #     print(text_model.make_short_sentence(140))
 
-
 # don't use stop words in the distance function
 def filter_sentence(sentence):
     return " ".join([word for word in sentence.split() if (word.lower() not in stopwords)])
 
+# scores a proposal sentence against a previous set of sentences for relevancy
+def score_sentence(input_sentences, proposal):
+    mask = [1.0, 1.0, 0.75, 0.75, 0.6525, 0.6525]
 
-def score_sentence(input_sentence, proposal):
+    total = 0
+    for i, (sentence, creator) in enumerate(input_sentences):
+        total += _score_sentence(sentence, proposal) * mask[i] * (1 if creator is 'human' else 1.0/3)
+
+# helper for score sentence, scores a proposal against a single input sentence
+def _score_sentence(input_sentence, proposal):
     input_sentence = input_sentence.replace(r'[^\w ]+', ' ').replace(r'\s+', ' ').strip()
     proposal = proposal.replace(r'[^\w ]+', ' ').replace(r'\s+', ' ').strip()
 
@@ -132,15 +133,21 @@ def score_sentence(input_sentence, proposal):
     # # return 1.0 * len(a.intersection(b)) / denom
     # return 1.0 * len(a.intersection(b))
 
+previous_sentences = []
+max_previous_sentences = 6
 
 # THIS WORKS
 input_sentence = "Malfoy took away Harry's broomstick."
+previous_sentences += [(input_sentence, 'human')]
 print(input_sentence)
 for _ in range(10):
-    proposals = [text_model.make_sentence() for i in not_tqdm(range(num_proposals))]
-    scores = [score_sentence(input_sentence, proposal) for proposal in not_tqdm(proposals)]
-    max_i, max_val = argmax(scores)
-    best_sentence = proposals[max_i]
+    proposals = [text_model.make_sentence() for i in range(num_proposals)]
+    scores = [score_sentence(previous_sentences, proposal) for proposal in proposals]
+    best_sentence = proposals[argmax(scores)]
+
+    previous_sentences.insert(0, (best_sentence, 'bot'))
+    if (len(previous_sentences) > max_previous_sentences):
+        previous_sentences.pop() # previous_sentences = previous_sentences[:-6]
     print(best_sentence)
 
 
